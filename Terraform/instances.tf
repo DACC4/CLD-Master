837ad4bc574c0a60b1f71ba84ad9f1e162f3f7ad
spiritclaw <lea@cherpillod.com>
2024-03-21 16:04:57 +0100

    Complete lab 2 part 3

--- Terraform/instances.tf
+++ Terraform/instances.tf
@@ -3,15 +3,58 @@ data "aws_key_pair" "kp_drupal" {
   include_public_key = true
 }
 
+data "aws_ami" "drupal" {
+  filter {
+    name = "image-id"
+    values = ["ami-067fbd29c40befdc0"]
+  }
+}
+
 resource "aws_instance" "drupal_a" {
-  ami = "ami-0fdefd1ed473b69ab"
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.subnet_a.id
-  private_ip = "10.0.3.10"
+  ami                    = data.aws_ami.drupal.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.subnet_a.id
+  private_ip             = "10.0.3.10"
   vpc_security_group_ids = [aws_security_group.sg_drupal.id]
-  key_name = data.aws_key_pair.kp_drupal.key_name
+  key_name               = data.aws_key_pair.kp_drupal.key_name
+
+  lifecycle {
+    ignore_changes = [ami]
+  }
 
   tags = {
     Name = "EC2_PRIVATE_DRUPAL_DEVOPSTEAM03_A"
+  }
+}
+
+resource "aws_instance" "drupal_b" {
+  ami                    = data.aws_ami.drupal.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.subnet_b.id
+  private_ip             = "10.0.3.140"
+  vpc_security_group_ids = [aws_security_group.sg_drupal.id]
+  key_name               = data.aws_key_pair.kp_drupal.key_name
+
+  lifecycle {
+    ignore_changes = [ami]
+  }
+
+  tags = {
+    Name = "EC2_PRIVATE_DRUPAL_DEVOPSTEAM03_B"
+  }
+}
+
+resource "aws_ec2_instance_state" "drupal_a" {
+  instance_id = aws_instance.drupal_a.id
+  state       = "running"
+}
+
+resource "aws_ami_from_instance" "drupal" {
+  name               = "AMI_DRUPAL_DEVOPSTEAM03_LABO02_RDS"
+  description        = "AMI_DRUPAL_DEVOPSTEAM03_LABO02_RDS"
+  source_instance_id = aws_instance.drupal_a.id
+
+  tags = {
+    Name = "AMI_DRUPAL_DEVOPSTEAM03_LABO02_RDS"
   }
 }
--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -1,26 +1,26 @@
 resource "aws_db_subnet_group" "drupal_sub_grp_rds" {
-  name       = "dbsubgrp-devopsteam03"
+  name        = "dbsubgrp-devopsteam03"
   description = "DBSUBGRP-DEVOPSTEAM03"
-  subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+  subnet_ids  = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
 }
 
 resource "aws_db_instance" "drupal_rds" {
-  engine = "mariadb"
+  engine         = "mariadb"
   engine_version = "10.11.7"
 
   identifier = "dbi-devopsteam03"
-  username = "admin"
-  password = var.drupal_rds_password
+  username   = "admin"
+  password   = var.drupal_rds_password
 
-  instance_class = "db.t3.micro"
-  storage_type = "gp3"
-  allocated_storage = "20"
+  instance_class        = "db.t3.micro"
+  storage_type          = "gp3"
+  allocated_storage     = "20"
   max_allocated_storage = 0
 
-  db_subnet_group_name = aws_db_subnet_group.drupal_sub_grp_rds.name
+  db_subnet_group_name   = aws_db_subnet_group.drupal_sub_grp_rds.name
   vpc_security_group_ids = [aws_security_group.sg_rds.id]
-  availability_zone = "eu-west-3a"
+  availability_zone      = "eu-west-3a"
 
-  monitoring_interval = 0
+  monitoring_interval     = 0
   backup_retention_period = 0
 }
